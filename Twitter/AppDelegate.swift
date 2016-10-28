903538022355ea30fc824b66bc5da04bfb329242
Jay Liew <jaysern@gmail.com>
2016-10-27 23:19:15 -0700

    bulk tweets initializer works

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -11,21 +11,22 @@ import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         TwitterClient.sharedInstance?.fetchAccessToken(withPath: "oauth/access_token",
                                                        method: "POST",
                                                        requestToken: BDBOAuth1Credential(queryString: url.query),
                                                        success: { (accessToken: BDBOAuth1Credential?) in
-                                                            print("--- ACCESS token success")
+                                                        
+                                                        print("--- ACCESS token success")
                                                         TwitterClient.sharedInstance?.requestSerializer.saveAccessToken(accessToken)
                                                         
-                                                        
                                                         TwitterClient.sharedInstance?.get(
-                                                            "1.1/account/verify_credentials.json",
+                                                            "1.1/statuses/home_timeline.json",
+                                                            //"1.1/account/verify_credentials.json",
                                                             parameters: nil,
                                                             progress: { (progress: Progress) in
                                                                 print("--- progress downloading")
@@ -33,52 +34,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                             success: { (dataTask: URLSessionDataTask, response: Any?) in
                                                                 print("--- SUCCESS GET")
                                                                 
-                                                                if let userDictionary = response as? NSDictionary{
-                                                                    let user = User(initDictionary: userDictionary)
-                                                                    print("--- username: " + user.name!)
+                                                                if let responseArray = response as? [Dictionary <String, Any>]{
+                                                                    let tweets = Tweet.tweetsWithArray(dictionaries: responseArray)
+                                                                    for t in tweets {
+                                                                        print(t.text!)
+                                                                    }
+                                                                    
                                                                 }
                                                                 
                                                             }, // success
                                                             failure: { (dataTask: URLSessionDataTask?, error: Error) in
                                                                 print("--- GET FAILURE")
                                                             } // failure
-                                                        ) // get
+                                                        ) // get data
                                                         
-                                                        }, // success fetch access
-                                                       failure: { (error: Error?) in
-                                                            print("--- ACCESS token fail")
-                                                        }
-        )
+                                                        
+            }, // success fetch access
+            failure: { (error: Error?) in
+                print("--- ACCESS token fail")
+            } // fail
+        ) // fetchAccessToken
         return true
-    }
-
+    } //UIApplicationOpenURLOptionsKey
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class Tweet: NSObject {
 
-}
+    var text: String?
+    var timestamp: Date?
+    var retweetCount: Int = 0
+    var favoritesCount: Int = 0
+    
+    init(dictionary: Dictionary <String, Any>){
+        text = dictionary["text"] as? String
+        retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+        favoritesCount = (dictionary["favorites_count"] as? Int) ?? 0
+        
+        let timestampString = dictionary["created_at"] as? String
+        
+        if let timestampString = timestampString{
+            let formatter = DateFormatter()
+            formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+            
+            timestamp = formatter.date(from: timestampString)
+        }
+    } // init
+    
+    class func tweetsWithArray(dictionaries: [Dictionary <String, Any>]) -> [Tweet] {
+        var tweets = [Tweet]()
+        for dictionary in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            tweets.append(tweet)
+        }
+        return tweets
+    }
+    
+} // Tweet
